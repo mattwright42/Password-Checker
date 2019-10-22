182e09a4ff0ec526a503c827c56fc027327ba14e
Matt Wright <matthewbwright@gmail.com>
2019-10-22 14:43:59 -0700

    Adds the switch cases to display password strength.

--- PasswordChecker/PasswordChecker.cs
+++ PasswordChecker/PasswordChecker.cs
@@ -16,7 +16,55 @@ namespace PasswordChecker
             string digits = "0123456789";
             string specialChars = "!@#$%^&*()-_+=\\/':,{}[]~.";
 
-            
+            Console.WriteLine("Please enter the password");
+            string password = Console.ReadLine();
+
+            int score = 0;
+
+            if (password.Length >= minLength)
+            {
+                score++;
+            }
+            if (password.Contains(lowercase))
+            {
+                score++;
+            }
+            if (password.Contains(uppercase))
+            {
+                score++;
+            }
+            if (password.Contains(digits))
+            {
+                score++;
+            }
+            if (password.Contains(specialChars))
+            {
+                score++;
+            }
+
+            Console.WriteLine("Your password score is: " + score);
+
+            switch(score)
+            {
+                case 5:
+                    Console.WriteLine("Congratulations! Your password is extrememly strong.");
+                    break;
+                case 4:
+                    Console.WriteLine("Congratualations! Your password is extrememly strong.");
+                    break;
+                case 3:
+                    Console.WriteLine("Congratulations! Your password is strong.");
+                    break;
+                case 2:
+                    Console.WriteLine("Your password is medium.");
+                    break;
+                case 1:
+                    Console.WriteLine("Your password is weak.");
+                    break;
+                default:
+                    Console.WriteLine("Your password does not meet the requirements. Please try again.");
+                    break;
+            }
         }
     }
 }
