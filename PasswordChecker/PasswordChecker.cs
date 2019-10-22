428b5da9bbfc2ae348d1bb52de91f2eb22455add
Matt Wright <matthewbwright@gmail.com>
2019-10-22 14:19:25 -0700

    Add project files.

---
+++ PasswordChecker/PasswordChecker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordChecker
+{
+    class PasswordChecker
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
