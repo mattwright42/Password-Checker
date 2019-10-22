c703b393b5dd9dba7a6ecf7006330604c145677d
Matt Wright <matthewbwright@gmail.com>
2019-10-22 14:26:17 -0700

    Creates variables to hold the required parts of the password

--- PasswordChecker/PasswordChecker.cs
+++ PasswordChecker/PasswordChecker.cs
@@ -10,7 +10,13 @@ namespace PasswordChecker
     {
         public static void Main(string[] args)
         {
+            int minLength = 8;
+            string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string lowercase = "abcdefghijklmnopqrstuvwxyz";
+            string digits = "0123456789";
+            string specialChars = "!@#$%^&*()-_+=\\/':,{}[]~.";
 
+            
         }
     }
 }
